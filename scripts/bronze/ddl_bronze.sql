1cd5e9500fd547ddb64e656a5a489433440c5f45
Mostafa Ahmed <104670507+Mostafahmed1@users.noreply.github.com>
2025-05-07 15:09:49 +0300

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,18 @@
+Create table Bronze.erp_px_cat_g1v2(
+	id				nvarchar(50),
+	car				nvarchar(50),
+	subcat			nvarchar(50),
+	maintenance		nvarchar(50)
+);
+GO
+Create table Bronze.erp_loc_a101(
+	cid				nvarchar(50),
+	cntry			nvarchar(50)
+);
+GO
+CREATE TABLE Bronze.cust_az12(
+	cid nvarchar(50),
+	bdate nvarchar(50),
+	gen nvarchar(50)
+);
+
