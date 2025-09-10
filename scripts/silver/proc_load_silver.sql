4988ba14277067878bc191c1baed71cac10ea29b
Mostafa Ahmed Abdallah <104670507+Mostafahmed1@users.noreply.github.com>
2025-09-10 14:21:05 +0300

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,9 +1,26 @@
--- Transformation Phase in Silver Layer
+/*
+================================================================================
+	Stored Procedure: Load Silver Layer (Bronze -> Silver)
+================================================================================
+Script Purpose:
+	This stored procedure performs the ETL (Extract, Transform, Load) process to 
+	populate the 'silver' schema tables from the 'bronze' schema.
+Actions Performed:
+	- Truncates Silver tables.
+	- Inserts transformed and cleansed data from Bronze into Silver tables.
 
--- Remove dublicates and nulls of a table
+Parameters:
+	None.
+	This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+	EXEC Silver.load_silver;
+*/
+
+
+-- Remove duplicates and nulls from a table
 -- Subquery idea is dealing with column alias as a column name
 -- Remove extra spaces by trim
-
 -- exec Silver.load_silver 
 
 
@@ -235,3 +252,4 @@ from Bronze.erp_loc_a101
 )*/
 
 --
+
