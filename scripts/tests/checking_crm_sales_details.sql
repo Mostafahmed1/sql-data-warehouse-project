d695cf65b59e35bc89ced950a3fbe26cdc2d4cdd
Mostafa Ahmed Abdallah <104670507+Mostafahmed1@users.noreply.github.com>
2025-09-10 14:30:08 +0300

    Update checking_crm_sales_details.sql

--- scripts/tests/checking_crm_sales_details.sql
+++ scripts/tests/checking_crm_sales_details.sql
@@ -1,4 +1,21 @@
--- Checks for NULLSs
+/*
+===========================================================
+Quality Checks
+===========================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy,
+    and standardization across the 'silver' schemas. It includes checks for: 
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    -Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+==========================================================
+*/
 select *
 from Bronze.crm_sales_details
 where sls_ord_num is Null  or  sls_prd_key is NULL or sls_cust_id is NULL
@@ -66,3 +83,4 @@ from (
 
 select * 
 from Silver.crm_sales_details
+
